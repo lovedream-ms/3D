f004418334907d9d03d8cadd383325bbf4742c6e
lovedream <2179417385@qq.com>
2026-02-11 10:42:09 +0800

    feat(YoloModel): 使用自定义预测器实现多任务推理
    - 支持detect, classify, segment, obb, pose多任务
    - 支持pt和om模型的推理(om模型未测试)
    
    BREAKING CHANGE: Yes

--- src/YoloModel.py
+++ src/YoloModel.py
@@ -1,122 +1,65 @@
-import cv2
-import numpy as np
 import torch
-from abc import ABC, abstractmethod
-from PIL import Image, ImageDraw, ImageFont
+import numpy as np
 
-from ais_bench.infer.interface import InferSession
-from ultralytics.utils.nms import non_max_suppression
-
-from config import CLASSES, colors
-
-FONT_PATH = "/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc"
-font = ImageFont.truetype(FONT_PATH, 20)
+from ultralytics.models.yolo.classify import ClassificationPredictor
+from ultralytics.models.yolo.detect import DetectionPredictor
+from ultralytics.models.yolo.obb import OBBPredictor
+from ultralytics.models.yolo.segment import SegmentationPredictor
+from ultralytics.models.yolo.pose import PosePredictor
 
 
-def drawBoundingBox(img, classId, confidence, x1, y1, x2, y2):
-    color = colors[classId]
-    cv2.rectangle(img, (x1, y1), (x2, y2), color, 1)
+class OmEngine:
+    """把昇腾推理封装成类似 PyTorch nn.Module 的接口"""
 
-    img_pil = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    draw = ImageDraw.Draw(img_pil)
+    def __init__(self, model_path):
+        from ais_bench.infer.interface import InferSession
 
-    label = f"{list(CLASSES.keys())[classId]} ({confidence:.2f})"
-    text_color_pil = tuple(color.astype(int)[::-1])
+        self.session = InferSession(device_id=0, model_path=model_path)
 
-    text_x = max(0, x1)
-    text_y = max(0, y1 - 25)
-
-    draw.text((text_x, text_y), label, font=font, fill=text_color_pil)
-
-    np.copyto(img, cv2.cvtColor(np.array(img_pil), cv2.COLOR_RGB2BGR))
+    def __call__(self, im):
+        raw_outputs = self.session.infer(feeds=[im.cpu().numpy()], mode="static")
+        return [torch.from_numpy(np.array(o)) for o in raw_outputs]
 
 
-class BaseYoloModel(ABC):
-    def __init__(self, modelPath, scoreMap=None, conf=0.25, iou=0.45, maxDet=20):
-        self.modelPath = modelPath
-        self.scoreMap = scoreMap
-        self.conf = conf
-        self.iou = iou
-        self.maxDet = maxDet
-        self._load_model()
+class YoloModel:
+    def __init__(self, model_path, task="detect", conf=0.25, iou=0.45, max_det=20):
+        self.task = task
+        self.is_om = model_path.endswith(".om")
 
-    @abstractmethod
-    def _load_model(self):
-        self.model = InferSession(device_id=0, model_path=self.modelPath)
+        # 定义所有任务对应的预测器类
+        self.pred_map = {
+            "class": ClassificationPredictor,
+            "detect": DetectionPredictor,
+            "obb": OBBPredictor,
+            "segment": SegmentationPredictor,
+            "pose": PosePredictor,
+        }
 
-    def warm_up(self):
-        testImage = np.zeros((640, 640, 3), dtype=np.uint8)
-        self.infer(testImage)
+        if self.is_om:
+            self.engine = OmEngine(model_path)
 
-    def _preprocess(self, originalImage, imgsz=640):
-        h, w, _ = originalImage.shape
-        length = max((h, w))
+            overrides = {"task": task, "conf": conf, "iou": iou, "max_det": max_det}
+            self.predictor = self.pred_map[task](overrides=overrides)
+            self.predictor.model = self.engine
+            self.predictor.setup_model(model=None)
+        else:
+            from ultralytics import YOLO
 
-        # 填充 (Padding) 为正方形并调整至 imgsz x imgsz
-        image = cv2.copyMakeBorder(
-            originalImage,
-            0,
-            length - h,
-            0,
-            length - w,
-            cv2.BORDER_CONSTANT,
-            value=(114, 114, 114),
-        )
+            self.model = YOLO(model_path)
+            self.model.predict(np.zeros((64, 64, 3), dtype=np.uint8), task=task)
+            self.predictor = self.model.predictor
 
-        # 归一化并转换为 blob (NCHW)
-        blob = cv2.dnn.blobFromImage(
-            image, scalefactor=1 / 255.0, size=(imgsz, imgsz), swapRB=True, crop=False
-        )
+    def predict(self, frame):
+        im = self.predictor.preprocess([frame])
+        preds = self.predictor.model(im)
+        return self.predictor.postprocess(preds, im, [frame])[0]
 
-        scale = length / imgsz
-        return blob, scale
 
-    def _postprocess(self, outputs, originalImage, scale):
-        """
-        后处理：包含 NMS 和坐标还原
-        """
-        preds = torch.from_numpy(outputs[0][0]).unsqueeze(0)
+if __name__ == "__main__":
+    import cv2
 
-        detections = non_max_suppression(
-            preds,
-            conf_thres=self.conf,
-            iou_thres=self.iou,
-            classes=None,
-            agnostic=False,
-            multi_label=False,
-            max_det=self.maxDet,
-        )
-
-        detectionFrame = originalImage.copy()
-        results = []
-
-        for det in detections:
-            if len(det) == 0:
-                continue
-
-            det[:, :4] *= scale
-
-            for *xyxy, conf, cls in det:
-                x1, y1, x2, y2 = map(int, xyxy)
-                class_id = int(cls)
-                score = float(conf)
-
-                drawBoundingBox(detectionFrame, class_id, score, x1, y1, x2, y2)
-
-                results.append(
-                    {
-                        "classId": class_id,
-                        "className": list(CLASSES.keys())[class_id],
-                        "score": score,
-                        "box": [x1, y1, x2, y2],
-                    }
-                )
-
-        return [d["className"] for d in results], detectionFrame
-
-    def infer(self, rgbFrame):
-        blob, scale = self._preprocess(rgbFrame)
-        outputs = self.model.infer(feeds=[blob], mode="static")
-        outputs = [np.array(output) for output in outputs]
-        classNames, detectionFrame = self._postprocess(outputs, rgbFrame, scale)
-        return classNames, detectionFrame
+    # 一次支持 pt/om, cls/detect/obb/segment/pose 全任务，其余任务均可以使用
+    model = YoloModel("models/yolov8n.pt", task="detect")
+    # model = YoloModel("models/yolov8n.om", task="detect")
+    result = model.predict(cv2.imread("data/test/0-0-0-0-0-1-180121.png"))
+    result.show()
